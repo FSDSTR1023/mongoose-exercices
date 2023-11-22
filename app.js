b4f880359b87800c5c0b54a76c03888092da3d5d
m-marinucci <mmarinucci@numinate.com>
2023-11-22 22:39:28 +0100

    connection does not work

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const port = 8000
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
+
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
+  console.log(process.env.DB_USER)
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
