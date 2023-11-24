31cbd14cedaab868502ea8b04d3b772c1b9558f2
m-marinucci <mmarinucci@numinate.com>
2023-11-24 21:03:20 +0100

    clase 24.11

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const port = 8000
 const mongoose = require("mongoose");
 const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
 
+app.use(express.json())
 
 async function main() {
   await mongoose.connect(mongoDB);
 }
 main().catch(err => console.log(err)); 
+
+var students = require('./routes/students.js')
+app.use('/students', students)
 
 
 app.get('/', (req, res) => {
